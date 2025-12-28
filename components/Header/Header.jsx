67bf48805fbb1cd849e419d9baa69a1792921a20
Sharad10-10 <sharad.ghimire2019@gmail.com>
2025-12-27 18:10:34 -0700

    Project setup and header and hero section completed

---
+++ components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FaBatteryFull, FaHome, FaWifi } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className='bg-[#FBF8FE] flex justify-between items-center px-6 py-1 border-b border-black/3 fixed w-full top-0 backdrop:blur-2xl'>
+      <div className='flex items-center gap-4'>
+        <FaHome />
+        <h1 className='text-lg'>Sharad Ghimire</h1>
+      </div>
+
+      <div className='flex gap-4 items-center text-[#7B818F]'>
+        <FaWifi className='text-blue-600'/>
+       <div className='flex items-center gap-2'>
+         <FaBatteryFull className='text-green-400'/>
+         <p className='text-sm'>100%</p>
+       </div>
+       <p className='text-sm'>4:40 PM</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ components/Introduction/Introduction.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import React from "react";
+import { MdEventNote } from "react-icons/md";
+
+const Introduction = () => {
+  const backgroundColor =
+    "bg-gradient-to-r from-[#E2EAF4] via-[#E7E9F3] to-[#ECE7F2] p-4";
+  return (
+    <div className={`${backgroundColor} min-h-screen flex items-center justify-center `}>
+        <div className="bg-[#F7FAFE] max-w-200 rounded-lg">
+          <div className="flex items-center p-2 px-6 border-b border-gray-700/10">
+            <div className="flex gap-2">
+              {/* colors */}
+              <div className="h-3 w-3 bg-[#F04242] rounded-full hover:scale-110 transition-all"></div>
+              <div className="h-3 w-3 bg-[#FBBD23] rounded-full hover:scale-110 transition-all"></div>
+              <div className="h-3 w-3 bg-[#26D968] rounded-full hover:scale-110 transition-all"></div>
+            </div>
+
+            <div className="text-center text-[#6A7280] flex-1">
+              {/*about me  */}
+              ~/about-me
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center text-center p-8">
+            <div className="flex flex-col justify-center items-center gap-4">
+              <div className="">
+                <Image
+                  src={"/portfolio-image.jpg"}
+                  alt="portfolio-image"
+                  height={150}
+                  width={150}
+                  className="rounded-4xl border-x-2 border-y-4 border-[#6A53E9]"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center gap-2">
+                <h1 className="text-3xl font-semibold">
+                  Hey, I'm{" "}
+                  <span className="text-[#764FE7]">Sharad Ghimire</span> 👋
+                </h1>
+                <h2 className="text-xl text-[#707786]">
+                  Fullstack Developer & Creative Technologist
+                </h2>
+                <h3 className="text-[#707786] flex items-center gap-2">
+                  <MdEventNote className="text-black" /> 2+ years experience
+                </h3>
+              </div>
+
+              <div className="text-center">
+                <p className="text-[#707786]">
+                  I craft pixel-perfect digital experiences with modern
+                  technologies. Passionate about clean code, intuitive
+                  interfaces, and building products that make a difference. When
+                  I'm not coding, you'll find me exploring places or playing
+                  games.
+                </p>
+              </div>
+
+              <div className="flex gap-4">
+                <button className="px-4 py-2 text-white bg-[#007BFF] rounded-lg hover:scale-105 transition-all cursor-pointer">
+                  Get in Touch
+                </button>
+                <button className="px-4 py-2 bg-[#E9EAED] rounded-lg hover:scale-105 transition-all cursor-pointer">
+                  View my work
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Introduction;
